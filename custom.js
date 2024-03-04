a82a31ab52d174ce226aadacd0654f39effed96b
Dipto_008 <142109281+Blankid018@users.noreply.github.com>
2024-03-04 18:39:55 +0000

    Add some files it's not well but working you will do this better

--- custom.js
+++ custom.js
@@ -1,10 +1,41 @@
-const logger = require('./utils/log');
 const cron = require('node-cron');
+const logger = require('./utils/log');
+const fs = require('fs-extra');
+//const PREFIX = true;
+/*
+const randomMessages = [
+  "Hello Everyone",
+  "Hello Everyone Gumawa naba kayo ng assignment niyo?",
+  "Hello Everyone Kamusta School Niyo?",
+  "Hello There I'm still alive",
+  "Hello Everyone Be Respectful to others Thanks you",
+  "How are you today?",
+  "Greetings all to Members",
+  "Hello Educator AI User Remember Don't Spam the Bot"
+];
+*/
+function randomMessage(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+function sendGreeting(api, messages, filePath) {
+  api.getThreadList(20, null, ['INBOX']).then((list) => {
+    list.forEach((thread) => {
+      if (thread.isGroup) {
+const dipto = fs.createReadStream(filePath);
+        api.sendMessage({body : randomMessage(messages),attachment: dipto}, thread.threadID).catch((error) => {
+          logger(`Error sending message: ${error}`, 'AutoGreet');
+        });
+      }
+    });
+  }).catch((error) => {
+    logger(`Error getting thread list: ${error}`, 'AutoGreet');
+  });
+}
 
 module.exports = async ({ api }) => {
-  const minInterval = 5;
+/*  const minInterval = 5;
   let lastMessageTime = 0;
-  let messagedThreads = new Set();
+  let messagedThreads = new Set();*/
 
   const config = {
     autoRestart: {
@@ -17,118 +48,67 @@ module.exports = async ({ api }) => {
       time: 30,
       note: 'Approve waiting messages after a certain time',
     },
+    greetings: [
+      {
+        cronTime: '0 6 * * *', // At 05:00 AM
+        messages: [`Good morning! Have a great day ahead!`],
+        filePath: "videos/5.mp4"
+      },
+      {
+        cronTime: '0 5 * * *', // At 05:00 AM
+        messages: [`°থাপ্পড়াইয়া কিডনী 𝙡𝙤𝙠 করে দিমু..!! 🐸🔪 Taratari ghum theke ut`],
+        filePath: "videos/4.mp4"
+      },
+      {
+        cronTime: '0 4 * * *', // At 04:00 AM
+        messages: [`তোমাকে ভালোবাসার কোন ব্যাখ্যা জানা নেই আমি,শুধু জানি মন থেকে ভালোবাসি আর ভালোবেসে যাবো!🙂🤎🖇`,`- Tomar GF nai মানেই তুমি আমার! 🥹🫶🙈🤭😁`,`— জীবনে একাকিত্ব'ই 𝐁𝐞𝐬𝐭-♡︎🩷🫰`],
+        filePath: "videos/3.mp4"
+      },
+      {
+        cronTime: '0 2 * * *', // At 02:00 AM
+        messages: [`Rat 2  baje gumaw sobay 🐤🐤`],
+        filePath: "videos/2.mp4"
+      },
+      {
+        cronTime: '0 0 * * *',// 12:00
+        messages: [`Ar akta din chole gelo,Taw tumi porta bosla na💩🐤`],
+        filePath : "videos/1.mp4"
+      }
+    ]
   };
 
-  function autoRestart(config) {
-    if (config.status) {
-      cron.schedule(`*/${config.time} * * * *`, () => {
-        logger.log('Start rebooting the system!', 'Auto Restart');
-        process.exit(1);
-      });
-    }
+  // Schedule predefined greeting messages
+  config.greetings.forEach((greeting) => {
+    cron.schedule(greeting.cronTime, async () => {sendGreeting(api, greeting.messages, greeting.filePath);
+    }, {
+      scheduled: false,
+      timezone: "Asia/Dhaka"
+    });
+  });
+  cron.schedule('*/15 * * * *', () => {
+   // sendGreeting(api, randomMessages);
+  }, {
+    scheduled: false,
+    timezone: "Asia/Dhaka"
+  }); 
+  // Auto-restart logic
+  if (config.autoRestart.status) {
+    cron.schedule(`*/${config.autoRestart.time} * * * *`, () => {
+      logger('Start rebooting the system!', 'Auto Restart');
+      process.exit(1);
+    });
   }
 
-  function acceptPending(config) {
-    if (config.status) {
-      cron.schedule(`*/${config.time} * * * *`, async () => {
-        const list = [
-          ...(await api.getThreadList(1, null, ['PENDING'])),
-          ...(await api.getThreadList(1, null, ['OTHER'])),
-        ];
-        if (list[0]) {
-          api.sendMessage('You have been approved for the queue. (This is an automated message)', list[0].threadID);
-        }
-      });
-    }
+  // Accept pending messages logic
+  if (config.acceptPending.status) {
+    cron.schedule(`*/${config.acceptPending.time} * * * *`, async () => {
+      const list = [
+        ...(await api.getThreadList(1, null, ['PENDING'])),
+        ...(await api.getThreadList(1, null, ['OTHER'])),
+      ];
+      if (list[0]) {
+        api.sendMessage('Hello', list[0].threadID);
+      }
+    });
   }
-
-  autoRestart(config.autoRestart);
-  acceptPending(config.acceptPending);
-
-  // AUTOGREET EVERY 10 MINUTES
-  cron.schedule('*/10 * * * *', () => {
-    const currentTime = Date.now();
-    if (currentTime - lastMessageTime < minInterval) {
-      console.log("Skipping message due to rate limit");
-      return;
-    }
-    api.getThreadList(25, null, ['INBOX'], async (err, data) => {
-      if (err) return console.error("Error [Thread List Cron]: " + err);
-      let i = 0;
-      let j = 0;
-
-      async function message(thread) {
-        try {
-          api.sendMessage({
-            body: `⟩ Thank you for using BotPack!\n\n⟩ Fork Here: https://replit.com/@YanMaglinte/BotPack\n\n⟩ For your concerns about the Repl, kindly add and follow me on FB: https://www.facebook.com/yandeva.me?mibextid=ZbWKwL`
-          }, thread.threadID, (err) => {
-            if (err) return;
-            messagedThreads.add(thread.threadID);
-
-          });
-        } catch (error) {
-          console.error("Error sending a message:", error);
-        }
-      }
-
-      while (j < 20 && i < data.length) {
-        if (data[i].isGroup && data[i].name != data[i].threadID && !messagedThreads.has(data[i].threadID)) {
-          await message(data[i]);
-          j++;
-          const CuD = data[i].threadID;
-          setTimeout(() => {
-            messagedThreads.delete(CuD);
-          }, 1000);
-        }
-        i++;
-      }
-    });
-  }, {
-    scheduled: false, // Set this to false to turn it off
-    timezone: "Asia/Manila"
-  });
-
-  // AUTOGREET EVERY 30 MINUTES
-  cron.schedule('*/30 * * * *', () => {
-    const currentTime = Date.now();
-    if (currentTime - lastMessageTime < minInterval) {
-      console.log("Skipping message due to rate limit");
-      return;
-    }
-    api.getThreadList(25, null, ['INBOX'], async (err, data) => {
-      if (err) return console.error("Error [Thread List Cron]: " + err);
-      let i = 0;
-      let j = 0;
-
-      async function message(thread) {
-        try {
-          api.sendMessage({
-            body: `Hey There! How are you? ヾ(＾-＾)ノ`
-          }, thread.threadID, (err) => {
-            if (err) return;
-            messagedThreads.add(thread.threadID);
-
-          });
-        } catch (error) {
-          console.error("Error sending a message:", error);
-        }
-      }
-
-
-      while (j < 20 && i < data.length) {
-        if (data[i].isGroup && data[i].name != data[i].threadID && !messagedThreads.has(data[i].threadID)) {
-          await message(data[i]);
-          j++;
-          const CuD = data[i].threadID;
-          setTimeout(() => {
-            messagedThreads.delete(CuD);
-          }, 1000);
-        }
-        i++;
-      }
-    });
-  }, {
-    scheduled: false, // Set this to false to turn it off
-    timezone: "Asia/Manila"
-  });
 };
---
+++ modules/commands/addfunction.js
@@ -0,0 +1,15 @@
+module.exports.config = {
+  name: "textt",
+  version: "",
+  hasPermssion: 0,
+  credits: "",
+  description: "",
+  usePrefix: true,
+  commandCategory: "",
+  usages: "",
+  cooldowns: 0
+};
+
+module.exports.run = async function({}){
+  const message= "it's not a command .I want tell you that add some function on botpack like global.utils.getStreamFromURL(); for stream download from link then message.reply or send.repy for message reply send for send message in thread .and also create your support group because all botpack users faces some issues .if you create a messager group then we discuss it .in Facebook you not respond all post so that i tell you that and keep update your Botpack like ntkhang.he update his project but you update you project after long day.whatever add this functions add also add approval option and coustom.js for video reply . Thank you .me dipto (fb-id => www.facebook.com/dipto008")
+}
