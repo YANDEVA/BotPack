3387939fe8d1154eee9cca367812e1127a914dff
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-05-05 08:24:41 +0800

    Create eval.js

---
+++ modules/commands/eval.js
@@ -0,0 +1,27 @@
+module.exports.config = {
+  name: "eval",
+  version: "1.0.0",
+  hasPermssion: 2,
+  credits: "Liane Cagara",
+  description: "Execute codes quickly.",
+  usePrefix: true,
+  commandCategory: "Admin",
+  usages: "[reply or text]",
+  cooldowns: 0,
+  dependencies: {
+
+  }
+};
+
+module.exports.run = async function({ api, args, event, box, Users, Threads, getText }) {
+  let code = args.join(" ");
+  if (event.messageReply) {
+    code = event.messageReply.body;
+  }
+  try {
+    await eval(code);
+  } catch (error) {
+    await box.reply(`❌ Error: ${error.message}`);
+    console.log(error);
+  }
+}
